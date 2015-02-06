07dcf6c7aa34ec4acf4c5b3ef457a6fd175cdeb2
a2anduaga <a2anduaga@gmail.com>
2015-02-06 13:00:25 +0100

    listViewFechas con mejoras (layout interno por cada item de lista)

---
+++ ListViewFechas/src/com/example/listviewfechas/Lista_entrada.java
@@ -0,0 +1,28 @@
+package com.example.listviewfechas;
+
+public class Lista_entrada {
+
+	private int idImagen;
+	private String fecha;
+	private String asunto;
+	
+	public Lista_entrada(int idImagen, String fecha, String asunto)
+	{
+		this.idImagen=idImagen;
+		this.fecha=fecha;
+		this.asunto=asunto;
+	}
+
+	public int getIdImagen() {
+		return idImagen;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+	
+}
--- ListViewFechas/src/com/example/listviewfechas/MainActivity.java
+++ ListViewFechas/src/com/example/listviewfechas/MainActivity.java
@@ -2,45 +2,59 @@ package com.example.listviewfechas;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends ListActivity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener{
 	
 	private Button bt;
 	private Spinner dia, mes, año;
 	private EditText et;
-	private String texto="";
 	private ArrayList<String> arrayDia = new ArrayList<String>();
 	private ArrayList<String> arrayAño = new ArrayList<String>();
-	private List<String> listaArray = null;
-	private ArrayAdapter<String> adaptador = null;
+	private ListView lista = null;
 	private Bbdd miBd;
+	// "entradas" es lo que recibes desde la Base de Datos
 	private ArrayList<String[]> entradas = new ArrayList<String[]>();
+	// Se volcaran las "entradas" en el ArrayList "datos" de tipo 'Lista_entrada'
+	private ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		listaArray = new ArrayList<String>();
-		adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaArray);
-		setListAdapter(adaptador);
+		
+		miBd = new Bbdd(this, "Entradas", null, 1);
+		entradas=miBd.selectAll();
+		Iterator<String[]> it = entradas.iterator();	
+		// Por cada row de la tabla, añade objeto de tipo "Lista_entrada" al arrayList "datos"
+		while(it.hasNext())
+		{	
+			String[] obj = it.next();
+			datos.add(new Lista_entrada(R.drawable.ic_launcher, obj[1]+"-"+obj[2]+"-"+obj[3], obj[4]));			
+		}
+		lista = (ListView)findViewById(R.id.listView);
+		
+		adaptador();
 		
 		bt=(Button)findViewById(R.id.aceptar);
 		bt.setOnClickListener(this);
 		et=(EditText)findViewById(R.id.et);
-		dia=(Spinner) findViewById(R.id.spinner1);
+		
 		for (int i=1; i<=31; i++)
 		{
 			arrayDia.add(String.valueOf(i));
@@ -49,6 +63,8 @@ public class MainActivity extends ListActivity implements OnClickListener{
 		{
 			arrayAño.add(String.valueOf(j));
 		}
+		// Los 3 spinners (dia - mes - año) y sus adaptadores (adapter1, 2 y 3)
+		dia=(Spinner) findViewById(R.id.spinner1);
 		ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayDia);
 		adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		dia.setAdapter(adapter1);
@@ -61,15 +77,17 @@ public class MainActivity extends ListActivity implements OnClickListener{
 		ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayAño);
 		adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		año.setAdapter(adapter3);
-		miBd = new Bbdd(this, "Entradas", null, 1);
-		entradas=miBd.selectAll();
-		Iterator<String[]> it = entradas.iterator();	
-		while(it.hasNext())
-		{	
-			String[] obj = it.next();
-			listaArray.add(obj[1]+"-"+obj[2]+"-"+obj[3]+"\r\n"+obj[4]);			
-		}
 		
+		// Borrar item del ListView si clickas sobre él
+		lista.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> pariente, View view, int posicion, long id)
+			{
+				// Borrar objeto del arrayList, actualizar adaptador de la lista y borrar de Base de Datos
+				datos.remove(posicion);
+				((Lista_adaptador)lista.getAdapter()).notifyDataSetChanged();
+				miBd.borrarEntrada(posicion);
+			}
+		});
 	}
 
 	@Override
@@ -96,19 +114,43 @@ public class MainActivity extends ListActivity implements OnClickListener{
 		
 		if (v.getId()==R.id.aceptar)
 		{
-			texto=dia.getSelectedItem().toString()+"-"+mes.getSelectedItem().toString()+"-"+año.getSelectedItem().toString()+"\r\n"+et.getText().toString();
+			// Insertar a la Base de Datos, añadir a arrayList y actualizar adpatador de la lista
 			miBd.insertarEntrada(dia.getSelectedItem().toString(), mes.getSelectedItem().toString(), año.getSelectedItem().toString(), et.getText().toString());
-			listaArray.add(texto);
-			adaptador.notifyDataSetChanged();
+			datos.add(new Lista_entrada(R.drawable.ic_launcher, dia.getSelectedItem().toString()+"-"+mes.getSelectedItem().toString()+"-"+año.getSelectedItem().toString(), et.getText().toString()));
+			((Lista_adaptador)lista.getAdapter()).notifyDataSetChanged();
 			et.setText("");
 		}
 		
 	}
-	protected void onListItemClick(ListView l, View v,final int position, long id)
-	{
-		listaArray.remove(position);
-		adaptador.notifyDataSetChanged();
-		miBd.borrarEntrada(position);
 
+	private void adaptador() {
+		// Enlazar adaptador a la ListView lista
+		lista.setAdapter(new Lista_adaptador(this, R.layout.entrada, datos)
+		{
+			// Por cada entrada (item) de la lista
+			public void onEntrada(Object entrada, View view)
+			{
+				if (entrada!=null)
+				{
+					// Por cada item, crea los dos TextView (fecha y asunto) y la imagen
+					TextView fecha = (TextView)view.findViewById(R.id.tvFecha);
+					if (fecha!=null)
+					{
+						fecha.setText(((Lista_entrada)entrada).getFecha());
+					}
+					TextView asunto = (TextView)view.findViewById(R.id.tvAsunto);
+					if (asunto!=null)
+					{
+						asunto.setText(((Lista_entrada)entrada).getAsunto());
+					}
+					ImageView imagen = (ImageView)view.findViewById(R.id.imagen);
+					if (imagen!=null)
+					{
+						imagen.setImageResource(((Lista_entrada)entrada).getIdImagen());
+					}
+				}
+			}
+		});
+		
 	}
 }
